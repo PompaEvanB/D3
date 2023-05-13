27a00c10ab287b05ac4017d5a50ee73ccd33ad78
PompaEvanB <pompa.evan.b@gmail.com>
2023-05-12 19:32:31 -0700

    Added better movement code for the redball
    Made it so the red ball will move with the mouse when clicked on. The red ball will also move when clicked on even after being dragged. To place the red ball back down again, simply click on it again.

--- game.js
+++ game.js
@@ -12,32 +12,53 @@ class G1 extends Phaser.Scene{
         let redBall = this.add.ellipse(100,100,75,75,0xff0000); // create the red ball that will be thrown
         redBall.setInteractive(); // make it interactable
 
-        let isDragging = false;
-        let isclicked = false;
-        let clicked = false;
+        // if the redball is clicked, it moves with the mouse. the next time it is clicked, it gets placed down.
+        // if the red ball is dragged, it will shoot from its current position in the opposite direction from the mouse.
 
-        // when the red ball is pressed
-        redBall.on('pointerdown', function(pointer){
-            isDragging = true;
-            isclicked = false;
-        });
+        let click = false; // check to see if the ball has been clicked on.
+        let clicked = false; // check to see if the ball has been clicked before.
+        let drag = false; // check to see if the ball is being dragged.
 
         // when the red ball is clicked on
+        redBall.on('pointerdown', function(pointer){
+            // tell the game that the ball has been clicked by changing to boolean
+            if(click){
+                click = false;
+            }
+            else{
+                click = true;
+            }
+            // set drag to true because we have not clicked off yet.
+            drag = true;
+        });
+
+        // when the red ball is clicked off of
         redBall.on('pointerup', function(pointer){
-            if(!clicked){
+            // if the ball has been clicked before, let the game know by setting clicked to false.
+            if(click){
                 clicked = true;
             }
             else{
                 clicked = false;
             }
-            isclicked = true;
-            isDragging = false;
-
+            // set drag to false because we clicked off of the ball.
+            drag = false;
         });
 
-        // when the mouse is moving  ADD DRAGGING RESETTING CLICK
+        // if at any point we mouse up, we cannot be dragging the ball.
+        this.input.on('pointerup', function(pointer){
+            drag = false;
+        });
+
+        // when the mouse is moving check to see if we should be moving the ball.
         this.input.on('pointermove', function(pointer){
-            if(isclicked && clicked){
+            // if we are dragging the mouse, we should not be moving the ball.
+            if(drag){
+                // reset both click and clicked to ensure we will always be able to move the ball after we drag.
+                click = false;
+                clicked = false;
+            }
+            if(clicked){ // if this is our first click, move the ball with the mouse.
                 redBall.x = pointer.x
                 redBall.y = pointer.y
             }
